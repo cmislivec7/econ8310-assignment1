ebf583e4088a363bdf417e6605b776082b1a1ffe
cmislivec7 <connormislivec@unomaha.edu>
2025-01-28 20:35:10 -0600

    20250128_834PM

--- assignment1.py
+++ assignment1.py
@@ -13,37 +13,38 @@ taxi_trips.index = data['Date']
 taxi_trips.index.freq = taxi_trips.index.inferred_freq
 
 # Add trend component
-trend = ExponentialSmoothing(taxi_trips, trend = 'add', seasonal = 'add').fit()
-
+#
+# model = ExponentialSmoothing(taxi_trips, trend = 'add', seasonal = 'add').fit()
+model =  ExponentialSmoothing(taxi_trips, trend = 'mul', seasonal = 'add', damped = True).fit(use_brute=True)
 #Linear trend with damping
 damptrend = ExponentialSmoothing(taxi_trips, trend = 'mul', seasonal = 'add', damped = True).fit(use_brute=True)
 
 prediction = len(pred)
-trend_fc = trend.forecast(prediction)
+modelFit = model.forecast(prediction)
 dtrend_fc = damptrend.forecast(prediction)
 
 import plotly.graph_objects as go
 
 # Plotting our data
 
-smoothData = pd.DataFrame([taxi_trips.values, trend.fittedvalues.values, damptrend.fittedvalues.values]).T
-smoothData.columns = ['Truth', 'Trend', 'Damped Trend']
+smoothData = pd.DataFrame([taxi_trips.values, model.fittedvalues.values, damptrend.fittedvalues.values]).T
+smoothData.columns = ['Truth', 'Model', 'Damped Trend']
 smoothData.index = taxi_trips.index
 
-fig = px.line(smoothData, y = ['Truth', 'Trend', 'Damped Trend'], 
+fig = px.line(smoothData, y = ['Truth', 'Model', 'Damped Trend'], 
         x = smoothData.index,
         color_discrete_map={"Truth": 'blue',
-                           'Trend': 'red',
+                           'Model': 'red',
                             'Damped Trend': 'green'
                            },
               title='Linear and Damped Trends'
        )
 
-fig.update_xaxes(range=[smoothData.index[-100], trend_fc.index[-1]])
+fig.update_xaxes(range=[smoothData.index[-100], modelFit.index[-1]])
 fig.update_yaxes(range=[0, 25000])
 
 
 # Incorporating the Forecasts
 
-fig.add_trace(go.Scatter(x=trend_fc.index, y = trend_fc.values, name='Forecast Trend', line={'color':'red'}))
+fig.add_trace(go.Scatter(x=modelFit.index, y = modelFit.values, name='Forecast Trend', line={'color':'red'}))
 fig.add_trace(go.Scatter(x=dtrend_fc.index, y = dtrend_fc.values, name='Forecast Damped Trend', line={'color':'green'}))
